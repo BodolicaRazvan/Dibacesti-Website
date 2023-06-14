42534de69c378a25999968c424398eb28fcad066
BodolicaRazvan <bodolicarazvan24@gmail.com>
2023-06-14 16:20:31 +0300

    Adding products+reviews section

--- script.js
+++ script.js
@@ -53,3 +53,25 @@ var swiper = new Swiper(".blogs-row", {
       },
     },
   });
+
+
+  const swiperEl = document.querySelector('swiper-container')
+  Object.assign(swiperEl, {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
+  swiperEl.initialize();
