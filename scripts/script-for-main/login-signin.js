21a747b69cdd7b9bdfe051d1d9276dfa84c438f2
BodolicaRazvan <bodolicarazvan24@gmail.com>
2023-07-10 18:49:47 +0300

    Adding Account Section

--- scripts/script-for-main/login-signin.js
+++ scripts/script-for-main/login-signin.js
@@ -1,23 +1,23 @@
 const formOpenBtn = document.querySelector("#form-open"),
-  home = document.querySelector(".home"),
+  account = document.querySelector(".account-container"),
   formContainer = document.querySelector(".form_container"),
   formCloseBtn = document.querySelector(".form_close"),
   signupBtn = document.querySelector("#signup"),
   loginBtn = document.querySelector("#login"),
   pwShowHide = document.querySelectorAll(".pw_hide");
 
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+formOpenBtn.addEventListener("click", () => account.classList.add("show"));
+formCloseBtn.addEventListener("click", () => account.classList.remove("show"));
 
 pwShowHide.forEach((icon) => {
   icon.addEventListener("click", () => {
     let getPwInput = icon.parentElement.querySelector("input");
     if (getPwInput.type === "password") {
       getPwInput.type = "text";
-      icon.classList.replace("fa-solid fa-eye-slash","fa-solid fa-eye");
+      icon.classList.replace("fa-eye-slash","fa-eye");
     } else {
       getPwInput.type = "password";
-      icon.classList.replace("fa-solid fa-eye", "fa-solid fa-eye-slash");
+      icon.classList.replace("fa-eye", "fa-eye-slash");
     }
   });
 });
@@ -26,6 +26,7 @@ signupBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.add("active");
 });
+
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.remove("active");
--- scripts/script-for-main/navbar-icons.js
+++ scripts/script-for-main/navbar-icons.js
@@ -1,23 +1,32 @@
 // Navbar Icons 
-let navBar = document.querySelector('.nav-bar');
+const navBar = document.querySelector('.nav-bar');
 document.querySelector('#menu-btn').onclick = () =>{
     navBar.classList.toggle('active');
     shopItems.classList.remove('active');
     searchBar.classList.remove('active');
+    if(window.scrollY == 0){
+        header.classList.toggle("sticky");
+    }
 }
 
-let shopItems = document.querySelector('.shop-container');
+const shopItems = document.querySelector('.shop-container');
 document.querySelector('#shop-btn').onclick = () =>{
     shopItems.classList.toggle('active');
     navBar.classList.remove('active');
     searchBar.classList.remove('active');
+    if(window.scrollY == 0){
+        header.classList.toggle("sticky");
+    }
 }
 
-let searchBar = document.querySelector('.search-bar');
+const searchBar = document.querySelector('.search-bar');
 document.querySelector('#search-btn').onclick = () =>{
     searchBar.classList.toggle('active');
     navBar.classList.remove('active');
     shopItems.classList.remove('active');
+    if(window.scrollY == 0){
+        header.classList.toggle("sticky");
+    }
 }
 
 window.onscroll = () =>{
